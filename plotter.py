8ccf2b96250781fcb053fd5b1236104ebca45229
Julia <julia.melgare@acad.pucrs.br>
2023-06-28 15:06:43 -0300

    adjust layout settings

--- plotter.py
+++ plotter.py
@@ -5,10 +5,13 @@ from data_loader import DataLoader
 
 subjects = DataLoader('data').load_data()
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 default_subject = 'E1'
 fig = px.imshow(cv.cvtColor(subjects[default_subject].get_image(), cv.COLOR_BGR2RGB))
+fig.update_layout(width=1366, height=768, margin=dict(l=10, r=10, b=10, t=10))
+fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+
 default_slider_max = len(subjects[default_subject].screen_images)-1
 default_slider_marks = {0: "0"}
 default_slider_marks[default_slider_max] = str(default_slider_max)
@@ -16,29 +19,35 @@ default_slider_marks[default_slider_max] = str(default_slider_max)
 current_subject = default_subject
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H3(children='User Visual Attention x Saliency Model Comparison'),
 
     html.Div(children='''
-        Dash: A web application framework for your data.
+        Developed by Julia Melgaré and Guido Mainardi for the Data Visualization class.
     '''),
 
-    html.Div(dcc.Graph(figure=fig, id='image-graph'), style={'width': '100%'}),
+    html.Div(
+        [
+            html.Div(dcc.Graph(figure=fig, id='image-graph'), style={'display': 'inline-block'}),
+            html.Div(
+                [
+                    html.Div(['Select Video:', dcc.Dropdown(list(subjects.keys()), default_subject, id='subject-dropdown')]),
+                    html.Br(),
+                    html.Div(['Show Maps:', dcc.Checklist(['User', 'Saliency Model'], [], id='maps-checklist')])
+                ],
+                style={'display': 'inline-block', 'verticalAlign': 'top'}
+            )
+        ], 
+        style={'width': '100%', 'display': 'flex', 'align-items':'top', 'justify-content':'center'}
+    ),
 
     html.Div(
         [
             html.Button('<', n_clicks=0, id='prev-btn', style={'display': 'inline-block', 'verticalAlign': 'top'}),
-            html.Div(dcc.Slider(0, default_slider_max, 1, value=0, marks=default_slider_marks, id='frame-slider'), style={'display': 'inline-block', 'width': '80%'}),
+            html.Div(dcc.Slider(0, default_slider_max, 1, value=0, marks=default_slider_marks, id='frame-slider'), style={'display': 'inline-block', 'width': '70%'}),
             html.Button('>', n_clicks=0, id='next-btn', style={'display': 'inline-block', 'verticalAlign': 'top'})
         ],
-        style={'width': '100%', 'display': 'flex', 'align-items':'center', 'justify-content':'center'}
-    ),
-    
-    html.Div(
-        [
-            html.Div(dcc.Dropdown(list(subjects.keys()), default_subject, id='subject-dropdown'), style={'width': '30%'}),
-            dcc.Checklist(['User', 'Saliency Model'], [], inline=True, id='maps-checklist')
-        ]
-    )
+        style={'align-items':'center', 'justify-content':'center', 'marginLeft' : '235px'}
+    )  
 ])
 
 @callback(
@@ -52,7 +61,10 @@ def update_image(frame, subject, checklist):
     subjects[subject].index = frame
     subjects[subject].heatmap_on if 'User' in checklist else subjects[subject].heatmap_off
     subjects[subject].saliency_on if 'Saliency Model' in checklist else subjects[subject].saliency_off
-    return px.imshow(cv.cvtColor(subjects[subject].get_image(), cv.COLOR_BGR2RGB))
+    fig = px.imshow(cv.cvtColor(subjects[subject].get_image(), cv.COLOR_BGR2RGB))
+    fig.update_layout(width=1366, height=768, margin=dict(l=10, r=10, b=10, t=10))
+    fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+    return fig
 
 @callback(
     Output('frame-slider', 'value'),
