3f7e814a17ab866c08ac002dc8c31d5fc37e7e4a
Julia <julia.melgare@acad.pucrs.br>
2023-06-28 01:54:41 -0300

    add functional slider and button controls

--- plotter.py
+++ plotter.py
@@ -1,16 +1,18 @@
 import plotly.express as px
-from dash import Dash, html, dcc, Input, Output, callback
-import cv2 as cv
-import os
+from dash import Dash, html, dcc, Input, Output, ctx, callback
 from data_loader import DataLoader
-
 
 subjects = DataLoader('data').load_data()
 
 app = Dash(__name__)
 
-default_fig = subjects['E1'].get_image()
-fig = px.imshow(default_fig)
+default_subject = 'E1'
+fig = px.imshow(subjects[default_subject].get_image())
+default_slider_max = len(subjects[default_subject].screen_images)-1
+default_slider_marks = {0: "0"}
+default_slider_marks[default_slider_max] = str(default_slider_max)
+
+current_subject = default_subject
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -19,24 +21,70 @@ app.layout = html.Div(children=[
         Dash: A web application framework for your data.
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
+    html.Div(dcc.Graph(figure=fig, id='image-graph'), style={'width': '100%'}),
+
     html.Div(
         [
-            dcc.Dropdown(['E1', 'E2', 'E3'], 'E1', id='demo-dropdown')
+            html.Button('<', n_clicks=0, id='prev-btn', style={'display': 'inline-block', 'verticalAlign': 'top'}),
+            html.Div(dcc.Slider(0, default_slider_max, 1, value=0, marks=default_slider_marks, id='frame-slider'), style={'display': 'inline-block', 'width': '80%'}),
+            html.Button('>', n_clicks=0, id='next-btn', style={'display': 'inline-block', 'verticalAlign': 'top'})
         ],
-        style={"width": "30%"},
+        style={'width': '100%', 'display': 'flex', 'align-items':'center', 'justify-content':'center'}
+    ),
+    
+    html.Div(
+        [
+            dcc.Dropdown(list(subjects.keys()), 'E1', id='subject-dropdown'),
+        ],
+        style={'width': '30%'},
     )
 ])
 
 @callback(
-    Output('example-graph', 'figure'),
-    Input('demo-dropdown', 'value')
+    Output('image-graph', 'figure'),
+    Input('frame-slider', 'value'),
+    Input('subject-dropdown', 'value')
 )
-def update_output(value):
-    return px.imshow(subjects[value].get_image())
+def update_image(frame, subject):
+    current_subject = subject
+    subjects[subject].index = frame
+    return px.imshow(subjects[subject].get_image())
+
+@callback(
+    Output('frame-slider', 'value'),
+    Input('subject-dropdown', 'value'),
+    Input('next-btn', 'n_clicks'),
+    Input('prev-btn', 'n_clicks'),
+)
+def update_slider_value(subject, next_btn, prev_btn):
+    if 'next-btn' == ctx.triggered_id:
+        subjects[subject].next
+        return subjects[subject].index
+    if 'prev-btn' == ctx.triggered_id:
+        subjects[subject].prev
+        return subjects[subject].index
+    return 0
+
+@callback(
+    Output('frame-slider', 'max'),
+    Output('frame-slider', 'marks'),
+    Input('frame-slider', 'value'),
+    Input('subject-dropdown', 'value')
+)
+def update_slider(frame, subject):
+    max = len(subjects[subject].screen_images)-1
+    marks = {0:'0'}
+    marks[frame] = str(frame)
+    marks[max] = str(max)
+    return max, marks
+
+@callback(
+    Output('next-btn', 'disabled'),
+    Output('prev-btn', 'disabled'),
+    Input('frame-slider', 'value')
+)
+def disable_buttons(frame):
+    return (frame >= len(subjects[current_subject].screen_images)-1), (frame <= 0)
 
 if __name__ == '__main__':
     app.run(debug=False)
